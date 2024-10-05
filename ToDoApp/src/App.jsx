c6cc065ad7b52c399a3cb31d9ecf107ab1882eaf
SachPlayZ <ssachinsingh99@gmail.com>
2024-10-05 15:45:33 +0530

    Made complete overhaul and added animations to todo

--- ToDoApp/src/App.jsx
+++ ToDoApp/src/App.jsx
@@ -1,39 +1,8 @@
-import React, { useState } from "react";
-import ToDoList from "./Components/ToDoList";
-import AddToDo from "./Components/AddToDo";
-import "./App.css";
+import React from "react";
+import AnimatedToDoApp from "./Components/AnimatedToDoApp";
 
-function App() {
-  const [tasks, setTasks] = useState([]);
-
-  const addTask = (task) => {
-    setTasks([...tasks, { text: task, completed: false }]);
-  };
-
-  const toggleTask = (index) => {
-    const newTasks = [...tasks];
-    newTasks[index].completed = !newTasks[index].completed;
-    setTasks(newTasks);
-  };
-
-  const deleteTask = (index) => {
-    const newTasks = tasks.filter((_, i) => i !== index);
-    setTasks(newTasks);
-  };
-
-  return (
-    <div className="App">
-      <div className="container">
-        <h1 className="title">Modern To-Do List</h1>
-        <AddToDo addTask={addTask} />
-        <ToDoList
-          tasks={tasks}
-          toggleTask={toggleTask}
-          deleteTask={deleteTask}
-        />
-      </div>
-    </div>
-  );
-}
+const App = () => {
+  return <AnimatedToDoApp />;
+};
 
 export default App;
